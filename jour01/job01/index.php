0221484f9594696a6f0621f64e1d4dd550b5c856
VALERIE21625 <valerie.kergonnan@laplateforme.io>
2025-09-16 14:10:52 +0200

    job1et 2

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+    <?php
+
+    $str="laplateforme";
+
+    
+
+    
+    $str2="vive";
+    echo $str2.$str ."<br>";
+
+    $val ="6";
+
+    echo $val+4 . "<br>";
+    $mybool="true";
+    $mybool="false";
+    echo $mybool;
+    ?>
---
+++ jour01/job03/index.php
@@ -0,0 +1,24 @@
+    <?php
+
+   $bool=true;
+   $entier=2;
+   $string="jem'aime";
+   $float=5.2;
+
+
+
+    ?>
+    <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td> </td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  
+</table>
+    
