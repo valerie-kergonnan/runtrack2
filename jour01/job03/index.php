2cd1b37524169b59c6cf9d858f26842f241f8f8d
VALERIE21625 <valerie.kergonnan@laplateforme.io>
2025-09-16 14:26:04 +0200

    job03

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -1,24 +1,33 @@
-    <?php
+<?php
 
-   $bool=true;
-   $entier=2;
-   $string="jem'aime";
-   $float=5.2;
+    $bool=true;
+    $entier=2;
+    $string="jem'aime";
+    $float=5.2;
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>job-03</title>
+    </head>
+    <body>
+           <table>
+                <tr>
+                    <th>type</th>
+                    <th>nom</th>
+                    <th>valeur</th>
+                    <th>decimal</th>
+                </tr>
+                <tr>
+                    <td><?php echo $bool; ?></td>
+                    <td><?php echo $entier; ?></td>
+                    <td><?php echo $string; ?></td>
+                    <td><?php echo $float; ?></td>              
+                </tr>
+            </table> 
+    </body>
+    </html>
 
-
-
-    ?>
-    <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td> </td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  
-</table>
     
